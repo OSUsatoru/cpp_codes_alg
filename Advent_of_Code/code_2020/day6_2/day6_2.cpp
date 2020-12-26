5590f45806518a0e79ed11ac22ad179cf053bd39
OSUsatoru <yamamsat@oregonstate.edu>
2020-12-26 01:33:52 -0800

    2020_day6

---
+++ Advent_of_Code/code_2020/day6_2/day6_2.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
+
+typedef long long ll;
+
+const int INF=2e9;
+const int MOD=1e9+7;
+
+const int PATTERN = 5;
+
+void Solve()
+{
+    ifstream ifs("input.txt");
+    if (ifs.fail()) {
+        cerr << "Failed to open file." << endl;
+        exit(1);
+    }
+    int index, key;
+    string str;
+    bool first = true;
+    vector<bool> check(26,false);
+    vector<int> cnts(1,0), each;
+    while(getline(ifs,str)){
+        if(str.empty()){
+            index = cnts.size()-1;
+            cnts[index] = count(check.begin(),check.end(),true);
+            cout << cnts[index] <<endl;
+            fill(check.begin(),check.end(), false);
+            cnts.push_back(0);
+            first = true;
+        }else{
+            if(first){
+                for(int i = 0;i < str.size(); ++i){
+                    key = str[i] - 'a';
+                    check[key] = true;
+                }
+                first = false;
+
+            }
+            else{
+                for(int i = 0;i < str.size(); ++i){
+                    key = str[i] - 'a';
+                    each.push_back(key);
+                }
+                for(int i = 0; i < 26; ++i){
+                    if(!count(each.begin(),each.end(),i) and check[i]){
+                        check[i] = false;
+                    }
+                }
+                each.clear();
+                each.shrink_to_fit();
+            }
+
+        }
+    }
+    //last group
+    index = cnts.size()-1;
+    cnts[index] = count(check.begin(),check.end(),true);
+    ofstream ofs("output.txt");
+    if (!ofs) {
+        cerr << "error" << std::endl;
+        exit(1);
+    }
+    ofs << accumulate(cnts.begin(),cnts.end(),0);
+    ifs.close();
+    ofs.close();
+
+
+}
+int main()
+{
+    fastio;
+    Solve();
+
+    return 0;
+}
