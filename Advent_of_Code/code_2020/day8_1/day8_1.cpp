29c57dd4ccfa4f1a99124a8a48947d0ad2458f76
OSUsatoru <yamamsat@oregonstate.edu>
2020-12-27 01:20:56 -0800

    2020_day8

--- Advent_of_Code/code_2020/day8_1/day8_1.cpp
+++ Advent_of_Code/code_2020/day8_1/day8_1.cpp
@@ -16,12 +16,44 @@ void Solve()
         cerr << "Failed to open file." << endl;
         exit(1);
     }
-
     ofstream ofs("output.txt");
     if (!ofs) {
         cerr << "error" << std::endl;
         exit(1);
     }
+
+    string str, key, value;
+    vector<pair<string,int>> operation;
+
+    while(getline(ifs, str)){
+        key = str.substr(0, str.find(' '));
+        value = str.substr(str.find(' ')+1);
+        if(value[0]=='+'){
+            operation.push_back(pair<string,int>(key,stoi(value.substr(1))));
+        }else{
+            operation.push_back(pair<string,int>(key,-(stoi(value.substr(1)))));
+        }
+    }
+    int index = 0, ans = 0;
+    vector<bool> check(operation.size(), false);
+    while(true){
+        if(check[index]){
+            break;
+        }else{
+            check[index]=true;
+        }
+        if(operation[index].first == "nop"){
+            ++index;
+        }else if(operation[index].first == "acc"){
+            ans+=operation[index].second;
+            ++index;
+        }else if(operation[index].first == "jmp"){
+            index+=operation[index].second;
+        }
+
+    }
+    ofs << ans;
+
 
     ifs.close();
     ofs.close();
--- Advent_of_Code/code_2020/day8_2/day8_2.cpp
+++ Advent_of_Code/code_2020/day8_2/day8_2.cpp
@@ -16,12 +16,71 @@ void Solve()
         cerr << "Failed to open file." << endl;
         exit(1);
     }
-
     ofstream ofs("output.txt");
     if (!ofs) {
         cerr << "error" << std::endl;
         exit(1);
     }
+
+    string str, key, value;
+    vector<pair<string,int>> operation;
+    deque<int> indexes;
+    int cnt = 0;
+    while(getline(ifs, str)){
+        key = str.substr(0, str.find(' '));
+        if(key == "jmp" or key == "nop"){
+            indexes.push_back(cnt);
+        }
+        value = str.substr(str.find(' ')+1);
+        if(value[0]=='+'){
+            operation.push_back(pair<string,int>(key,stoi(value.substr(1))));
+        }else{
+            operation.push_back(pair<string,int>(key,-(stoi(value.substr(1)))));
+        }
+        cnt++;
+    }
+
+    int index = 0, ans = 0, tmp;
+    bool ok;
+    vector<bool> check(operation.size(), false);
+    operation[indexes[0]].first = (operation[indexes[0]].first == "nop" ? "jmp":"nop");
+    while(true){
+
+        if(check[index] or index >= operation.size()){
+            ans = 0;
+            index = 0;
+            fill(check.begin(),check.end(),false);
+            if(indexes.size() >=2){
+                //fix previous one and and change next one
+                operation[indexes[0]].first = (operation[indexes[0]].first == "nop" ? "jmp":"nop");
+                operation[indexes[1]].first = (operation[indexes[1]].first == "nop" ? "jmp":"nop");
+                indexes.pop_front();
+            }else{
+                ok = false;
+                break;
+            }
+        }else{
+
+            check[index]=true;
+        }
+        if(operation[index].first == "nop"){
+            ++index;
+        }else if(operation[index].first == "acc"){
+            ans+=operation[index].second;
+            ++index;
+        }else if(operation[index].first == "jmp"){
+            index+=operation[index].second;
+        }
+        if(index == operation.size() ){
+            ok = true;
+            break;
+        }
+    }
+
+    if(ok)
+        ofs << ans;
+    else
+        ofs << "no";
 
     ifs.close();
     ofs.close();
